47a74edfa13cce0a91fe5a115701454cda38844f
Heron <darkgezar@gmail.com>
2021-11-08 17:32:26 +0200

    Start

---
+++ Assets/Scripts/CardManagerScr.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public struct Card
+{
+    public string Name;
+    public Sprite Logo;
+    public int Attack, Defense;
+
+    public Card(string name, string logoPath, int attack, int defense)
+    {
+        Name = name;
+        Logo = Resources.Load<Sprite>(logoPath);
+        Attack = attack;
+        Defense = defense;
+
+    }
+}
+
+public class Ca
+public class CardManagerScr : MonoBehaviour
+{
+
+}
